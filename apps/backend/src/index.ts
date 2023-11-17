fb14a24896b9db90394b34766cc99ef482da5984
Milan Suk <Milansuk@email.cz>
2023-11-17 23:13:14 +0100

    feat: backend structure

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,46 +1,18 @@
-import * as Middleware from "@effect/platform/Http/Middleware";
-import * as ServerResponse from "@effect/platform/Http/ServerResponse";
-import * as ServerRequest from "@effect/platform/Http/ServerRequest";
-import { NodeServer, RouterBuilder } from "effect-http";
-import { api } from "@effect-http-example/api";
-import { Effect } from "effect";
+import { NodeServer } from "effect-http";
+import { Config, Effect, LogLevel, Logger, pipe } from "effect";
 import { runMain } from "@effect/platform-node/Runtime";
 import { PrettyLogger } from "effect-log";
+import { app } from "./app.js";
 
-const corsMiddleware = (_allowedOrigins: readonly string[] | string) => {
-  const allowedOrigins =
-    typeof _allowedOrigins === "string" ? [_allowedOrigins] : _allowedOrigins;
+const ServerConfig = Config.all({
+  port: Config.number("PORT").pipe(Config.withDefault(3000)),
+});
 
-  return Middleware.make((app) =>
-    Effect.gen(function* (_) {
-      const request = yield* _(ServerRequest.ServerRequest);
-      let response = yield* _(app);
-
-      const url = request.headers["origin"] || request.url;
-
-      if (allowedOrigins.includes(url)) {
-        response = response.pipe(
-          ServerResponse.setHeaders({
-            "Access-Control-Allow-Origin": url,
-          }),
-        );
-      }
-
-      return response;
-    }),
-  );
-};
-
-const app = RouterBuilder.make(api).pipe(
-  RouterBuilder.handle("getItems", () =>
-    Effect.succeed([{ name: "test", value: 69 }]),
-  ),
-  RouterBuilder.build,
-  corsMiddleware(["http://localhost:3001", "http://localhost:3002"]),
-);
-
-app.pipe(
-  NodeServer.listen({ port: 3000 }),
+const main = pipe(
+  Effect.config(ServerConfig),
+  Effect.flatMap((config) => app.pipe(NodeServer.listen({ port: config.port }))),
   Effect.provide(PrettyLogger.layer()),
-  runMain,
+  Logger.withMinimumLogLevel(LogLevel.Warning)
 );
+
+runMain(main);
